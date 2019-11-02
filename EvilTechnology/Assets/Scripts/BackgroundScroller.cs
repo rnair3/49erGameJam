1b31509d0ef20841438b56b1652dd20e3d7e1686
Rinita Nair <rnair3@uncc.edu>
2019-11-02 15:14:33 -0400

    Initial Commit

---
+++ EvilTechnology/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed;
+    Material material;
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(scrollSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ EvilTechnology/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float jumpSpeed = 2f;
+
+
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        Vector2 velocity = new Vector2(horizontalThrow * speed, rigidbody.velocity.y);
+        rigidbody.velocity = velocity;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            rigidbody.velocity += jumpVelocity;
+        }
+
+    }
+}
